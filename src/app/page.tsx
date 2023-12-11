d9701df5a9580a4cf556a85c547b52128a9e43d5
tranvankhuyen <khuyentvhe187268@fpt.edu.vn>
2023-12-12 00:17:53 +0700

    🐛 fix: deploy bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
 import { Hero } from "@/section/Hero";
-import { Gallery } from "@/section/Gallery";
 import React from "react";
 
 export default function Home() {
-
-
   return (
         <div className="">
             <Hero />
             <div className="max-w-7xl mx-auto px-4 md:px-12 lg:px-20 pb-20 space-y-20 mt-20">
-                <Gallery />
+                {/*<Gallery />*/}
             </div>
         </div>
   )
