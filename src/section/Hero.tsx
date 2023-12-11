8d7eaeef870101509674c66aa49fff0f60ec897e
tranvankhuyen <khuyentvhe187268@fpt.edu.vn>
2023-12-11 16:47:43 +0700

    💄 update: text size

--- src/section/Hero.tsx
+++ src/section/Hero.tsx
@@ -13,10 +13,13 @@ export function Hero() {
     }
     return (
         <div className="hero relative top-0 bottom-0 my-auto mx-0 z-5 bg-black">
-            <span className="z-3 absolute w-full text-center top-0 bottom-0 my-auto mx-0 text-white flex items-center justify-center text-6xl font-bold">
+            <span className="z-3 absolute w-full text-center top-0 bottom-0 my-auto mx-0 text-white flex items-center justify-center text-4xl md:text-6xl font-bold">
                     <Typewriter
                         onInit={(typewriter) => {
                             typewriter.typeString('Câu lạc bộ Kĩ sư phần mềm Nhật Bản')
+                                .pauseFor(1500)
+                                .deleteAll()
+                            typewriter.typeString('Japanese Software Engineers')
                                 .pauseFor(1500)
                                 .deleteAll()
                             typewriter.typeString('クラブ。日本の　エンジニア')
