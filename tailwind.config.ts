fb1fa9225112ee63d3eca08d8b9778e4ddb758e5
tranvankhuyen <khuyentvhe187268@fpt.edu.vn>
2023-12-12 00:52:07 +0700

    ✨ add: config destop screen

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,16 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    screens: {
+        'md': '640px',
+        // => @media (min-width: 640px) { ... }
+
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+    }
   },
   plugins: [],
 }
