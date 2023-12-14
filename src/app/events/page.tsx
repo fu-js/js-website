76bfcf24c3ddd5fe8ed14daccba1073e0360c1e3
tranvankhuyen <khuyentvhe187268@fpt.edu.vn>
2023-12-14 14:36:04 +0700

    🎉 add: events page

---
+++ src/app/events/page.tsx
@@ -0,0 +1,14 @@
+import { Hero } from "@/section/Hero";
+import React from "react";
+import {CurrentEvent} from "@/section/CurrentEvent";
+
+export default function Home() {
+  return (
+        <div className="">
+            <Hero />
+            <div className="max-w-7xl mx-auto px-4 md:px-12 lg:px-20 pb-20 space-y-20 mt-20">
+
+            </div>
+        </div>
+  )
+}
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -15,6 +15,7 @@ export function Navigation({ className, func }: NavigationProps) {
             <Link onClick={func} className="border-b-2 border-t-2 border-red-800 w-[90%] text-center p-5" href="/">Trang chủ</Link>
             <Link onClick={func} className="border-b-2 border-red-800 w-[90%] text-center p-5" href="/about">Giới thiệu</Link>
             <Link onClick={func} className="border-b-2 border-red-800 w-[90%] text-center p-5" href="/history">Lịch sử CLB</Link>
+            <Link onClick={func} className="border-b-2 border-red-800 w-[90%] text-center p-5" href="/events">Sự kiện</Link>
             <Link onClick={func} className="border-b-2 border-red-800 w-[90%] text-center p-5" href="/products">Sản phẩm</Link>
             <Link onClick={func} className="border-b-2 border-red-800 w-[90%] text-center p-5" href="/achievements">Thành tựu</Link>
         </div>
